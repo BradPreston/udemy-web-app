4a4ca44c248307fcd1757fee543c9ee570240ec7
bradpreston <bap5393@gmail.com>
2023-05-05 13:40:00 -0400

    add jwt handlers and endpoints

---
+++ cmd/api/api-handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	// read a json payload
+	// look up user by email address
+	// check password
+	// generate token if password matches
+	// send token to user
+}
+
+func (app *application) refresh(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) allUsers(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) getUser(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) updateUser(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) deleteUser(w http.ResponseWriter, r *http.Request) {}
+
+func (app *application) insertUser(w http.ResponseWriter, r *http.Request) {}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,17 +9,36 @@ import (
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
-	
+
 	// register middleware
 	mux.Use(middleware.Recoverer)
 
 	// mux.Use(app.enableCORS)
 
-	// authentication routes - auth handler, refresh
+	// authentication routes
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh-token", app.refresh)
 
 	// test handler
+	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		var payload = struct {
+			Message string `json:"message"`
+		}{
+			Message: "hello world",
+		}
+		_ = app.writeJSON(w, http.StatusOK, payload)
+	})
 
-	// protected routes 
+	// protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		// use auth middleware
+
+		mux.Get("/", app.allUsers)
+		mux.Post("/", app.insertUser)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/{userID}", app.updateUser)
+	})
 
 	return mux
 }
